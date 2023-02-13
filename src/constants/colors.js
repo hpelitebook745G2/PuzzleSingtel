6d95d188432ca59134703497246231c9c7a1cfef
Earl Grant Reyes <reyes@noisolation.com>
2023-02-13 23:42:04 +0800

    Added reusable colors

---
+++ src/constants/colors.js
@@ -0,0 +1,14 @@
+export default {
+  primary: '#ed1a3d',
+  secondary: '#231f20',
+  inActiveBtn: '#BFBFBF',
+  white: '#FFFFFF',
+  border: '#4B4B49',
+  buttonBg: '#FCEFDF',
+  buttonBorder: '#f1a561',
+  disabledGray: '#E8E8E8',
+  disabledGrayTransparent: '#E8E8E888',
+  overlay: '#F5FCFF88',
+  subHeaderText: '#767676',
+  lightGray: '#F5F5F5',
+};
