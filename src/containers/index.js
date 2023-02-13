956dd9180a7bf84ad5284f1283fffd450e58572f
Earl Grant Reyes <reyes@noisolation.com>
2023-02-13 23:46:57 +0800

    Indexed screens containers folder

---
+++ src/containers/index.js
@@ -0,0 +1,13 @@
+import DashboardScreen from './DashboardScreen';
+import GameScreen from './GameScreen';
+import InputPlayerScreen from './InputPlayerScreen';
+import LeaderboardScreen from './LeaderboardScreen';
+import SuccessScreen from './SuccessScreen';
+
+export {
+  InputPlayerScreen,
+  DashboardScreen,
+  GameScreen,
+  LeaderboardScreen,
+  SuccessScreen,
+};
