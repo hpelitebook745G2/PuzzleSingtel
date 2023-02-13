bb6f816d4d029cf831baf9440cd561b7d8906821
Earl Grant Reyes <reyes@noisolation.com>
2023-02-13 23:26:35 +0800

    Updated ESLint rules + Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,38 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es2021: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    indent: 'off',
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-empty-function': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
