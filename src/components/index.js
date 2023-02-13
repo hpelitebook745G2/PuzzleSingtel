da07dc4d79f5683863e6bbc79d743335d874fe3b
Earl Grant Reyes <reyes@noisolation.com>
2023-02-13 23:38:12 +0800

    Indexed reusable components

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import BottomComponent from './BottomComponent';
+import Button from './Button';
+import EmptyList from './EmptyList';
+import Header1Text from './Header1Text';
+import Header2Text from './Header2Text';
+import PlayerRow from './PlayerRow';
+
+export {
+  Button,
+  BottomComponent,
+  Header1Text,
+  Header2Text,
+  PlayerRow,
+  EmptyList,
+};
