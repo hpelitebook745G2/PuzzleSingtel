95e1c0ac9b366df4e09105fa3d7d4786dcf19810
Earl Grant Reyes <reyes@noisolation.com>
2023-02-13 23:48:28 +0800

    Added users reducer

---
+++ src/reducers/UsersReducer.js
@@ -0,0 +1,31 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+  players: [
+    {name: 'Earl', score: 1500},
+    {name: 'Fat', score: 653},
+    {name: 'Grant', score: 234},
+    {name: 'Riley', score: 1500},
+    {name: 'Tim', score: 653},
+    {name: 'Lyn', score: 234},
+  ],
+  // players: [],
+  currentPlayer: '',
+};
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {
+    setPlayers: (state, action) => {
+      state.players = action.payload;
+    },
+    setCurrentPlayer: (state, action) => {
+      state.currentPlayer = action.payload;
+    },
+  },
+});
+
+export const {setPlayers, setCurrentPlayer} = usersSlice.actions;
+
+export default usersSlice.reducer;
