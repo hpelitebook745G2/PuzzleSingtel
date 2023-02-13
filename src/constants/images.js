4a4cd09a18441c069e1c6397af27885bba9a8810
Earl Grant Reyes <reyes@noisolation.com>
2023-02-13 23:44:18 +0800

    Added image static sources

---
+++ src/constants/images.js
@@ -0,0 +1,5 @@
+export default {
+  CHECK: require('@/assets/icons/check.png'),
+  EMPTY_LEADERBOARD: require('@/assets/icons/empty_leaderboard.png'),
+  AVATAR: require('@/assets/icons/avatar.png'),
+};
