f50042a20bd7754326db208455a26f70f03ff8a0
Earl Grant Reyes <reyes@noisolation.com>
2023-02-14 03:40:38 +0800

    Added currentScore

--- src/reducers/UsersReducer.js
+++ src/reducers/UsersReducer.js
@@ -1,16 +1,17 @@
 import {createSlice} from '@reduxjs/toolkit';
 
 const initialState = {
-  players: [
-    {name: 'Earl', score: 1500},
-    {name: 'Fat', score: 653},
-    {name: 'Grant', score: 234},
-    {name: 'Riley', score: 1500},
-    {name: 'Tim', score: 653},
-    {name: 'Lyn', score: 234},
-  ],
-  // players: [],
+  // players: [
+  //   {name: 'Earl', score: 1500},
+  //   {name: 'Fat', score: 653},
+  //   {name: 'Grant', score: 234},
+  //   {name: 'Riley', score: 1500},
+  //   {name: 'Tim', score: 653},
+  //   {name: 'Lyn', score: 234},
+  // ],
+  players: [],
   currentPlayer: '',
+  currentScore: 0,
 };
 
 const usersSlice = createSlice({
@@ -23,9 +24,13 @@ const usersSlice = createSlice({
     setCurrentPlayer: (state, action) => {
       state.currentPlayer = action.payload;
     },
+    setCurrentScore: (state, action) => {
+      state.currentScore = action.payload;
+    },
   },
 });
 
-export const {setPlayers, setCurrentPlayer} = usersSlice.actions;
+export const {setPlayers, setCurrentPlayer, setCurrentScore} =
+  usersSlice.actions;
 
 export default usersSlice.reducer;
